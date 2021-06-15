817ba0906dcd434d21d73b99859ab320b310762b
dmssantos <dms.santos_@hotmail.com>
2021-06-15 15:04:03 -0300

    Created Link

---
+++ nextjs-blog/pages/post/first-post.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function FirstPost(){
+  return(
+    <>
+
+      <h1>First Post</h1>
+
+      <h2>
+        <Link href="/">
+          <a>back to home</a>
+        </Link>
+      </h2>
+
+    </>
+  )
+}
