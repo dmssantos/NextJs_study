c833f73f6063521d79b749e5ed224bf25f5ad076
dmssantos <dms.santos_@hotmail.com>
2021-06-15 17:30:06 -0300

    Created style css

--- nextjs-blog/pages/post/first-post.js
+++ nextjs-blog/pages/post/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost(){
   return(
-    <>
+    <Layout>
+     <Head>
+       <title>First Post</title>
+     </Head>
 
       <h1>First Post</h1>
 
@@ -11,7 +16,6 @@ export default function FirstPost(){
           <a>back to home</a>
         </Link>
       </h2>
-
-    </>
+    </Layout>
   )
 }
